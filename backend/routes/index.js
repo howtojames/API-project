33823120c184d1afbc16ffb3c9ee5a841d725485
James Ruan <jrjamesruan@gmail.com>
2023-09-24 03:45:18 -0700

    render test3

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,14 +13,15 @@ const apiRouter = require('./api');  //every single middleware in the api/ folde
 //sending the text, Hello World! as the response's body.
 
 //---------------------------------
+// Moved to the bottom in Frontend - Render
 // Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-});
+// router.get("/api/csrf/restore", (req, res) => {
+//     const csrfToken = req.csrfToken();
+//     res.cookie("XSRF-TOKEN", csrfToken);
+//     res.status(200).json({
+//       'XSRF-Token': csrfToken
+//     });
+// });
 //This route should not be available in production, but it will not be exclusive to the production application until you implement the frontend of the application later. So for now, it will remain available to both the development and production environments.
 
 //---------------------------------
