d52574c397a30460c00c0ab26dbf7e32ccb80858
James Ruan <jrjamesruan@gmail.com>
2023-11-17 11:10:25 -0800

    phase1 test4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,47 @@
 // frontend/src/App.jsx
 // changed in phase 1
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+///----------------------------
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';  //added Outlet phase 1
 import LoginFormPage from './components/LoginFormPage/LoginFormPage';  //added this for Component because we don't have index.js
+//------------------------------
+import * as sessionActions from './store/session';
 
+//------
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);  //trackes whether user has be loaded or not, so App only loads if restoreUser is returned
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => {
+      setIsLoaded(true)
+    });
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
+//------
+
+//added Layout in phase 1
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <h1>Welcome!</h1>
-  },
-  {
-    path: '/login',
-    element: <LoginFormPage />
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <h1>Welcome!</h1>
+      },
+      {
+        path: '/login',
+        element: <LoginFormPage />
+      }
+    ]
   }
 ]);
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -36,6 +36,16 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+//phase 1
+//restore session user thunk
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+  // ...
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
